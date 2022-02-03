4aa3a295ce9ff7b02a60141b942e40e8c66317aa
JakobFrench <90063498+JakobFrench@users.noreply.github.com>
2022-02-03 14:00:36 -0600

    Add files via upload

---
+++ pa1_321/ISing.cs
@@ -0,0 +1,11 @@
+using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace pa1_321
+{
+    public interface ISing
+    {
+        public int CompareTo(object temp);
+    }// never used
+}
